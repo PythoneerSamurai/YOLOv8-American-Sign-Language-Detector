c8fd994a1efc26bf7678d6bb97da30c2b95246b0
MCVD <haroonrashidcc3@gmail.com>
2024-02-19 22:39:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO  # If you don't have ultralytics, install it by typing "pip install ultralytics" on the terminal and hit enter
 
-MODEL_PATH = "./Sign-Language-Object-Detector/runs/detect/train/weights/last.pt"  # Specify absolute path to last.pt or best.pt
+MODEL_PATH = "./American-Sign-Language-Alphabets-Object-Detector/runs/detect/train/weights/last.pt"  # Specify absolute path to last.pt or best.pt
 
 MODEL = YOLO(MODEL_PATH)
 
